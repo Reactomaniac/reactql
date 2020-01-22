bd8dcbd03afa2f696237abe97909079eb6f5b9f6
Junaid Farooq <junaid@evercam.io>
2020-01-22 17:24:12 +0500

    Added Mongoose && Start saving data to Mongo Schema.

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 
+const mongoose = require("mongoose");
+
+const Event = require("./models/event")
+
 const app = express();
 
 const events = [];
@@ -42,22 +46,40 @@ app.use("/graphql", graphqlHttp({
     `),
   rootValue: {
     events: () => {
-      return events;
+      return Event.find().then(events => {
+        return events.map(event => {
+          return { ...event._doc };
+        })
+      }).catch(err => {
+        throw err
+      })
     },
     createEvent: args => {
-      const event = {
-        _id: Math.random().toString(),
+      const event = new Event({
         title: args.eventInput.title,
         description: args.eventInput.description,
         price: +args.eventInput.price,
-        date: args.eventInput.date
-      };
-      events.push(event);
-      return event;
+        date: new Date(args.eventInput.date)
+      });
+      return event
+        .save()
+        .then(result => {
+          console.log(result);
+          return { ...result._doc };
+        })
+        .catch(err => {
+          console.log(err);
+          throw err;
+        });
     }
   },
   graphiql: true
 })
 );
 
-app.listen(3000);
+mongoose.connect(`mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ds211709.mlab.com:11709/events`)
+  .then( () => {
+    app.listen(3000);
+  }).catch(err => {
+  console.log(err);
+})
