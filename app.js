481c0242c9e5adec9e156837bb98a7f9a011a2ba
Junaid Farooq <junaid@evercam.io>
2020-01-23 08:24:09 +0500

    Throw Error when user already exists.

--- app.js
+++ app.js
@@ -24,12 +24,14 @@ app.use("/graphql", graphqlHttp({
       description: String!
       price: Float!
       date: String!
+      creator: User!
     }
 
     type User {
       _id: ID!
       email: String!
       password: String!
+      createdEvents: [Event!]
     }
 
     input EventInput {
@@ -87,14 +89,19 @@ app.use("/graphql", graphqlHttp({
         });
     },
     createUser: args => {
-      return bcrypt.hash(args.userInput.password, 12).then(hashedPassword => {
+      return User.findOne({email: args.userInput.email}).then(user => {
+        if (user) {
+          throw new Error("User exists already.")
+        }
+        return bcrypt.hash(args.userInput.password, 12)
+      }).then(hashedPassword => {
         const user = new User({
           email: args.userInput.email,
           password: hashedPassword
         })
         return user.save();
       }).then(result => {
-        return { ...result._doc, _id: result.id };
+        return { ...result._doc, password: null, _id: result.id };
       }).catch(err => {
         throw err;
       })
