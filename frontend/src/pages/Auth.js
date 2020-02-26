e50836b05d52e3d38e103e4a317ed0c79ac4cae2
Junaid Farooq <junaid@evercam.io>
2020-02-26 17:45:48 +0500

    Created Auth Page.

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,12 +1,90 @@
-import React, { Component } from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import { CardHeader } from '@material-ui/core';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import TextField from '@material-ui/core/TextField';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 
-class AuthPage extends Component {
-  render() {
-    return (
-      <>
-      </>
-      );
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: 500,
+    margin: "0 auto",
+    marginTop: 10
+  },
+  margin: {
+    margin: theme.spacing(1),
+  },
+  widthInput: {
+    width: 400
+  },
+  button: {
+    textTransform: "capitalize"
+  },
+  actions: {
+    width: 400,
+    margin: "0 auto"
   }
-}
+}));
 
-export default AuthPage;
+export default function AuthPage() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader align="center" title="Sign In" />
+      <CardContent align="center">
+        <TextField
+          className={classes.margin + " " + classes.widthInput}
+          id="input-with-icon-textfield"
+          label="Email"
+          type="email"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          className={classes.margin + " " + classes.widthInput}
+          id="input-with-icon-textfield"
+          label="Password"
+          type="password"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <VpnKeyIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </CardContent>
+      <CardActions
+        align="center"
+        className={classes.actions}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.button}
+        >
+          Sign In
+        </Button>
+        <Button
+          className={classes.button}
+          variant="contained"
+        >
+          Sign up
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
