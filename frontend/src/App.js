9f95ae39c4c08d31686613a5510b43d9306b9fe8
Junaid Farooq <junaid@evercam.io>
2020-02-26 16:45:38 +0500

    Added Material UI and Setup menu.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
-
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 import AuthPage from "./pages/Auth";
 import BookingsPage from "./pages/Bookings";
 import EventsPage from "./pages/Events";
 import MainNavigation from "./components/Navigation/MainNavigation";
-
 import './App.css';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "70%",
+    margin: "0 auto"
+  }
+}))
+
 function App() {
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
       <React.Fragment>
-        <div className="wrapper">
+        <Grid className={classes.root}>
           <MainNavigation />
           <div className="main-panel">
             <Switch>
@@ -22,7 +31,7 @@ function App() {
               <Route path="/bookings" component={BookingsPage} />
             </Switch>
           </div>
-        </div>
+        </Grid>
       </React.Fragment>
     </BrowserRouter>
   );
--- frontend/src/components/Navigation/MainNavigation.js
+++ frontend/src/components/Navigation/MainNavigation.js
@@ -1,11 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import EventIcon from '@material-ui/icons/Event';
+import BookmarksIcon from '@material-ui/icons/Bookmarks';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
 
+const useStyles = makeStyles({
+  root: {
+    width: 500,
+    margin: "0 auto"
+  },
+});
 
-const MainNavigation = (props) => {
+export default function MainNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
   return (
-    <div>
-    </div>
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Events" icon={<EventIcon />} />
+      <BottomNavigationAction label="Bookings" icon={<BookmarksIcon />} />
+      <BottomNavigationAction label="Auth" icon={<LockOpenIcon />} />
+    </BottomNavigation>
   );
 }
-
-export default MainNavigation;
